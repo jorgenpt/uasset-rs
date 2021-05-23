6fefdc1c678d2fd62ca31c1c6c525caae06f4bc2
Jørgen P. Tjernø <jorgen@tjer.no>
2021-05-23 11:42:55 -0700

    Some initial crate-level docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,48 @@
+//! [![github]](https://github.com/jorgenpt/uasset-rs)&ensp;[![crates-io]](https://crates.io/crates/uasset)&ensp;[![docs-rs]](https://docs.rs/uasset)
+//!
+//! [github]: https://img.shields.io/badge/github-8da0cb?style=for-the-badge&labelColor=555555&logo=github
+//! [crates-io]: https://img.shields.io/badge/crates.io-fc8d62?style=for-the-badge&labelColor=555555&logo=rust
+//! [docs-rs]: https://img.shields.io/badge/docs.rs-66c2a5?style=for-the-badge&labelColor=555555&logoColor=white&logo=data:image/svg+xml;base64,PHN2ZyByb2xlPSJpbWciIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgdmlld0JveD0iMCAwIDUxMiA1MTIiPjxwYXRoIGZpbGw9IiNmNWY1ZjUiIGQ9Ik00ODguNiAyNTAuMkwzOTIgMjE0VjEwNS41YzAtMTUtOS4zLTI4LjQtMjMuNC0zMy43bC0xMDAtMzcuNWMtOC4xLTMuMS0xNy4xLTMuMS0yNS4zIDBsLTEwMCAzNy41Yy0xNC4xIDUuMy0yMy40IDE4LjctMjMuNCAzMy43VjIxNGwtOTYuNiAzNi4yQzkuMyAyNTUuNSAwIDI2OC45IDAgMjgzLjlWMzk0YzAgMTMuNiA3LjcgMjYuMSAxOS45IDMyLjJsMTAwIDUwYzEwLjEgNS4xIDIyLjEgNS4xIDMyLjIgMGwxMDMuOS01MiAxMDMuOSA1MmMxMC4xIDUuMSAyMi4xIDUuMSAzMi4yIDBsMTAwLTUwYzEyLjItNi4xIDE5LjktMTguNiAxOS45LTMyLjJWMjgzLjljMC0xNS05LjMtMjguNC0yMy40LTMzLjd6TTM1OCAyMTQuOGwtODUgMzEuOXYtNjguMmw4NS0zN3Y3My4zek0xNTQgMTA0LjFsMTAyLTM4LjIgMTAyIDM4LjJ2LjZsLTEwMiA0MS40LTEwMi00MS40di0uNnptODQgMjkxLjFsLTg1IDQyLjV2LTc5LjFsODUtMzguOHY3NS40em0wLTExMmwtMTAyIDQxLjQtMTAyLTQxLjR2LS42bDEwMi0zOC4yIDEwMiAzOC4ydi42em0yNDAgMTEybC04NSA0Mi41di03OS4xbDg1LTM4Ljh2NzUuNHptMC0xMTJsLTEwMiA0MS40LTEwMi00MS40di0uNmwxMDItMzguMiAxMDIgMzguMnYuNnoiPjwvcGF0aD48L3N2Zz4K
+//!
+//! # The Rust UAsset Library
+//!
+//! `uasset-rs` is a pure Rust implementation of the Unreal Engine `.uasset` file format.
+//! It gives you direct access to fields & values in the uasset format, and is intended
+//! to allow you to build tools outside of the Unreal Editor to work with uassets.
+//!
+//! ## Usage
+//!
+//! To use `uasset-rs`, first add this to your `Cargo.toml`:
+//!
+//! ```toml
+//! [dependencies]
+//! uasset = "^0.1"
+//! ```
+//!
+//! Then import [`PackageFileSummary`] into your program:
+//!
+//! ```rust
+//! use uasset::PackageFileSummary;
+//! ```
+//!
+//! Finally, parse a file using [`PackageFileSummary::new`].
+//!
+//! ## Example
+//!
+//! ```rust
+//! # use uasset::{PackageFileSummary, Result};
+//! # use std::{fs::File, path::PathBuf};
+//! # fn main() -> Result<()> {
+//! # let path = PathBuf::from("assets/UE410/SimpleRefs/SimpleRefsRoot.uasset");
+//! let file = File::open(path)?;
+//! let package = PackageFileSummary::new(&file)?;
+//! for import in package.package_import_iter() {
+//!     println!("Import: {}", import);
+//! }
+//! # Ok(())
+//! # }
+//! ```
+
 mod enums;
 mod error;
 mod serialization;
