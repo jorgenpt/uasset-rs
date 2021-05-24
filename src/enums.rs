e26871b4815b65026549dca4075726e15492da0d
Jørgen P. Tjernø <jorgen@tjer.no>
2021-05-23 17:11:28 -0700

    Remove two `#[allow(dead_code)]` entries
    This code is no long dead.

--- src/enums.rs
+++ src/enums.rs
@@ -1,6 +1,6 @@
 /// Maps to `EUnrealEngineObjectUE4Version` in Engine/Source/Runtime/Core/Public/UObject/ObjectVersion.h -- this tracks
 /// the various versions to the core serialized object format.
-#[allow(non_camel_case_types, dead_code)]
+#[allow(non_camel_case_types)]
 #[derive(Debug)]
 pub enum ObjectVersion {
     VER_UE4_OLDEST_LOADABLE_PACKAGE = 214,
@@ -316,7 +316,6 @@ pub enum ObjectVersion {
 
 /// Maps to `EPackageFlags` in Engine/Source/Runtime/CoreUObject/Public/UObject/ObjectMacros.h. The `UnusedN` entries
 /// exist for completeness, presumably those entries were used in the past.
-#[allow(dead_code)]
 #[derive(Debug)]
 pub enum PackageFlags {
     None = 0x00000000,
