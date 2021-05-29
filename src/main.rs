361179f831db8e552e0c14a37eae3426c6771e69
Jørgen P. Tjernø <jorgen@tjer.no>
2021-05-29 12:18:56 -0700

    CLI: Add error handling to all code paths

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
-use std::{fs::File, path::PathBuf, time};
+use anyhow::Result;
+use log::{error, trace};
+use simplelog::{Config, TermLogger, TerminalMode};
+use std::{
+    fs::File,
+    path::{Path, PathBuf},
+    time,
+};
 use structopt::StructOpt;
+use structopt_flags::LogLevel;
 use uasset::AssetHeader;
 use walkdir::WalkDir;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "uasset", about = "A program to display various uasset data")]
 struct CommandOptions {
+    #[structopt(flatten)]
+    verbose: structopt_flags::QuietVerbose,
     #[structopt(subcommand)]
     cmd: Command,
 }
@@ -57,8 +67,34 @@ fn recursively_walk(paths: Vec<PathBuf>) -> Vec<PathBuf> {
         .collect()
 }
 
-fn main() {
+fn try_parse<T: FnOnce(AssetHeader)>(asset_path: &Path, callback: T) -> bool {
+    trace!("reading {}", asset_path.display());
+    match File::open(asset_path) {
+        Ok(file) => match AssetHeader::new(file) {
+            Ok(header) => {
+                callback(header);
+                true
+            }
+            Err(error) => {
+                error!("failed to parse {}: {:?}", asset_path.display(), error);
+                false
+            }
+        },
+        Err(error) => {
+            error!("failed to load {}: {:?}", asset_path.display(), error);
+            false
+        }
+    }
+}
+
+fn main() -> Result<()> {
     let options = CommandOptions::from_args();
+    TermLogger::init(
+        options.verbose.get_level_filter(),
+        Config::default(),
+        TerminalMode::Mixed,
+    )?;
+
     match options.cmd {
         Command::Benchmark {
             assets_or_directories,
@@ -69,19 +105,29 @@ fn main() {
 
             let load_start = time::Instant::now();
             let num_assets = asset_paths.len();
-            let num_imports: usize = asset_paths
+            let (num_errs, num_imports) = asset_paths
                 .into_iter()
                 .map(|asset_path| {
-                    let file = File::open(asset_path).unwrap();
-                    let header = AssetHeader::new(&file).unwrap();
-                    header.imports.len()
+                    let mut num_imports = 0;
+                    let reader = |header: AssetHeader| {
+                        trace!("found {} imports", header.imports.len());
+                        num_imports = header.imports.len();
+                    };
+
+                    if try_parse(&asset_path, reader) {
+                        (0, num_imports)
+                    } else {
+                        (1, 0)
+                    }
                 })
-                .sum();
+                .fold((0, 0), |(sum_errs, sum_imports), (errs, imports)| {
+                    (sum_errs + errs, sum_imports + imports)
+                });
             let load_duration = load_start.elapsed();
 
             println!(
-                "Loading {} assets with {} imports took {:?}",
-                num_assets, num_imports, load_duration,
+                "Loading {} assets ({} failed) with {} imports took {:?}",
+                num_assets, num_errs, num_imports, load_duration,
             );
             println!("Total execution took {:?}", start.elapsed());
         }
@@ -90,11 +136,11 @@ fn main() {
         } => {
             let asset_paths = recursively_walk(assets_or_directories);
             for asset_path in asset_paths {
-                println!("{}:", asset_path.display());
-                let file = File::open(asset_path).unwrap();
-                let header = AssetHeader::new(&file).unwrap();
-                println!("{:#?}", header);
-                println!();
+                try_parse(&asset_path, |header| {
+                    println!("{}:", asset_path.display());
+                    println!("{:#?}", header);
+                    println!();
+                });
             }
         }
         Command::ListImports {
@@ -103,15 +149,17 @@ fn main() {
         } => {
             let asset_paths = recursively_walk(assets_or_directories);
             for asset_path in asset_paths {
-                println!("{}:", asset_path.display());
-                let file = File::open(asset_path).unwrap();
-                let header = AssetHeader::new(file).unwrap();
-                for import in header.package_import_iter() {
-                    if !skip_code_imports || !import.starts_with("/Script/") {
-                        println!("  {}", import);
+                try_parse(&asset_path, |header| {
+                    println!("{}:", asset_path.display());
+                    for import in header.package_import_iter() {
+                        if !skip_code_imports || !import.starts_with("/Script/") {
+                            println!("  {}", import);
+                        }
                     }
-                }
+                });
             }
         }
     }
+
+    Ok(())
 }
